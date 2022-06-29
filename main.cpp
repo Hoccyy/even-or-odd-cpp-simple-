e2d861bfe202202ab5c9669c202ed7e54a03378d
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2022-06-28 21:28:33 -0400

    main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void Even_Odd(int num){
+    cout << ((num %2 == 0)? ("Even"): ("Not even"));
+}
+
+int main(){
+    Even_Odd(7); //Change your value here
+}
